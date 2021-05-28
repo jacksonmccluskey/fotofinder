52793bcb51de17403ae06e9109e469baca0f1242
jacksonmccluskey <jacksonmccluskey@protonmail.com>
2021-05-28 16:20:10 -0400

    final project uncut

--- fotofinder/src/spelling/Words.js
+++ fotofinder/src/spelling/Words.js
@@ -1,30 +1,29 @@
 const axios = require('axios')
 
-const ROOT_URL = 'https://gist.githubusercontent.com/anonymous/4d4ccc05ee8dfa637dc9e47548e90372/raw/2ced47226cbad8b1353a9afbb8593ade4d077267/wordlist.json'
+// UNIX words
 
-const regexVowels = /[aeiou]/g
-const regexAlpha = /[^A-Za-z]/g
+const url = 'https://gist.githubusercontent.com/anonymous/4d4ccc05ee8dfa637dc9e47548e90372/raw/2ced47226cbad8b1353a9afbb8593ade4d077267/wordlist.json'
+
+// regex declarations
+
+const regexVowels = /[aeiou]/g // used to identify vowels
+const regexAlpha = /[^A-Za-z]/g // used to identify non-alpha characters
 
 // retrieve UNIX words data
 
 const getData = async () => {
     try {
-        const {data} = await axios.get(ROOT_URL);
-        console.log("getData: try")
-        console.log(data)
-        return data;
+        const { data } = await axios.get(url) // retrieve UNIX words from gist
+        return data
     } catch (err) {
-        console.log(err.message);
+        console.log(err.message)
     }
 }
 
 // compare each word with term
 
 const checkWords = (wordData, searchTerm) => {
-    let finalWord = "Not Found" // corrected word
-
-    console.log("checkWords: wordData")
-    console.log(wordData)
+    let finalWord = "Earth" // corrected word
 
     for (let i = 0; i < wordData.length; i++) { // first loop: check if searchTerm is in dictionary
         if (wordData[i] === searchTerm) { // searchTerm is equal to word in dictionary
@@ -33,27 +32,26 @@ const checkWords = (wordData, searchTerm) => {
         }
     }
 
-    console.log("checkWords: finalWord")
-    console.log(finalWord)
-    console.log(typeof(finalWord))
+    if (finalWord !== "Earth") return finalWord // word is spelled correctly
 
-    if (finalWord !== "Not Found") return finalWord // word is spelled correctly
+    let possibleWords = []
+    let possibleDistances = []
 
     for (let i = 0; i < wordData.length; i++) { // second loop: check if word matches searchTerm (non-vowels)
         if (matchConsonants(wordData[i], searchTerm)) { // searchTerm is equal to word in dictionary (non-vowels)
-            finalWord = wordData[i] // assign finalWord to current word in dictionary
-            break
+            const distance = levenshteinDistance(wordData[i], searchTerm)
+            possibleWords.push(wordData[i])
+            possibleDistances.push(distance)
+            finalWord = wordData[i]
         }
     }
 
-    console.log("checkWords: finalWord")
-    console.log(finalWord)
-    console.log(typeof(finalWord))
+    finalWord = getClosestWord(possibleWords, possibleDistances)
 
     return finalWord
 }
 
-// mask both terms to compare non-vowel letters
+// mask both terms by replacing all vowels to compare non-vowel letters
 
 const matchConsonants = (word, searchTerm) => {
     const lowerCaseWord = word.toLowerCase() // transform both words to lowercase
@@ -65,28 +63,76 @@ const matchConsonants = (word, searchTerm) => {
     return hashtagWord === hashtagSearchTerm // compare non-vowel letters
 }
 
+// levenshtein distance formula to find closest match
+
+const levenshteinDistance = (a, b) => {
+    if(a.length === 0) return b.length
+    if(b.length === 0) return a.length
+
+    let matrix = []
+
+    // increment along the first column of each row
+    let i = 0
+    for(i = 0; i <= b.length; i++){
+        matrix[i] = [i]
+    }
+
+    // increment each column in the first row
+    let j = 0
+    for(j = 0; j <= a.length; j++){
+        matrix[0][j] = j
+    }
+
+    // Fill in the rest of the matrix
+    for(i = 1; i <= b.length; i++){
+        for(j = 1; j <= a.length; j++){
+            if(b.charAt(i-1) === a.charAt(j-1)){
+                matrix[i][j] = matrix[i-1][j-1];
+            } else {
+                matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                               Math.min(matrix[i][j-1] + 1, // insertion
+                               matrix[i-1][j] + 1)) // deletion
+                }
+            }
+        }
+
+    return matrix[b.length][a.length];
+}
+
+const getClosestWord = (words, distances) => {
+    const smallestIndex = indexOfSmallest(distances)
+
+    if (smallestIndex === -1) return "Earth"
+
+    return words[smallestIndex]
+}
+
+const indexOfSmallest = (distances) => {
+    let smallestIndex = -1
+
+    if (distances.length === 0) return smallestIndex
+
+    const smallestDistance = Math.min(...distances)
+    
+    if (distances) smallestIndex = distances.indexOf(smallestDistance)
+
+    return smallestIndex
+}
+
 // spell checker, remove non-alpha, compare with all words
 
 const spellChecker = async (searchTerm) => {
-    const correctedSearchTerm = searchTerm.replace(regexAlpha, '')
+    const correctedSearchTerm = searchTerm.replace(regexAlpha, '') // remove non-alpha
 
-    const data = await getData()
+    const data = await getData() // retrieve word data
 
-    console.log("spellChecker: getData()")
-    console.log(data)
-
-    const word = checkWords(data, correctedSearchTerm)
-
-    console.log("spellChecker: word")
-    console.log(word)
-
-    return word
+    return checkWords(data, correctedSearchTerm) // return corrected term
 }
+
+// run the spellChecker function on searchTerm
 
 const runSpellChecker = async (searchTerm) => {
     return await spellChecker(searchTerm)
 }
-
-runSpellChecker("citias7").then((correctedTerm) => {console.log(correctedTerm)})
 
 module.exports = { runSpellChecker }
